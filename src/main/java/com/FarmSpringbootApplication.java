c344640e370ce34cdcc94a06efd7328fe3be717f
sutton <50450947+Ioome@users.noreply.github.com>
2023-04-02 15:20:44 +0800

    sutton
    修改项目基本结构

---
+++ src/main/java/com/FarmSpringbootApplication.java
@@ -0,0 +1,27 @@
+package com;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+
+/**
+ * @author: sutton
+ * @date: 2023/4/2
+ * @description: SpringbootSchemaApplication
+ */
+@SpringBootApplication
+@MapperScan(basePackages = {"com.dao"})
+public class FarmSpringbootApplication extends SpringBootServletInitializer {
+
+    public static void main (String[] args) {
+        SpringApplication.run(FarmSpringbootApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure (SpringApplicationBuilder applicationBuilder) {
+        return applicationBuilder.sources(FarmSpringbootApplication.class);
+    }
+}
