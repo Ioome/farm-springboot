7913320b03af4dcfafb534060efe3632ba3bdc45
sutton <50450947+Ioome@users.noreply.github.com>
2023-04-02 17:16:02 +0800

    增加日志

--- src/main/java/com/farm/FarmSpringbootApplication.java
+++ src/main/java/com/farm/FarmSpringbootApplication.java
@@ -1,10 +1,18 @@
 package com.farm;
 
+import cn.hutool.core.util.StrUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 
 /**
@@ -13,11 +21,25 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
  * @description: SpringbootSchemaApplication
  */
 @SpringBootApplication
+@EnableSwagger2
+@Slf4j
 @MapperScan(basePackages = "com.farm.dao")
 public class FarmSpringbootApplication extends SpringBootServletInitializer {
 
-    public static void main (String[] args) {
-        SpringApplication.run(FarmSpringbootApplication.class, args);
+    public static void main (String[] args) throws UnknownHostException {
+        ConfigurableApplicationContext run = SpringApplication.run(FarmSpringbootApplication.class, args);
+        System.out.println("-----------------------------------");
+        Environment env = run.getEnvironment();
+        String ip = InetAddress.getLocalHost().getHostAddress();
+        String port = env.getProperty("server.port");
+        String path = env.getProperty("server.servlet.context-path");
+        path = StrUtil.isNotEmpty(path) ? path:"";
+        log.info("\n----------------------------------------------------------\n\t"
+                + "Application Jeecg-Boot is running! Access URLs:\n\t" + "Local: \t\thttp://localhost:" + port + path
+                + "/\n\t" + "External: \thttp://" + ip + ":" + port + path + "/\n\t" + "swagger-ui: \thttp://" + ip
+                + ":" + port + path + "/swagger-ui.html\n\t" + "Doc: \t\thttp://" + ip + ":" + port + path
+                + "/doc.html\n" + "----------------------------------------------------------");
+        log.info("启动成功 V0.0.1{}", System.currentTimeMillis());
     }
 
     @Override
