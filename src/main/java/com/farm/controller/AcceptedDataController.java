ab7633efca2153be6bcbe8e4a7ea53b9ab5ab6c3
sutton <50450947+Ioome@users.noreply.github.com>
2023-04-21 14:40:53 +0800

    sutton
    增加一个接受数据控制器

---
+++ src/main/java/com/farm/controller/AcceptedDataController.java
@@ -0,0 +1,10 @@
+package com.farm.controller;
+
+/**
+ * @name: AcceptedDataController
+ * @author: sutton
+ * @date: 2023-04-21 14:40
+ * @description: AcceptedDataController
+ */
+public class AcceptedDataController{
+}
